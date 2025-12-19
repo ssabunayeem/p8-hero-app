2b48d04aa1e582d4dd446ff8e6e3b5793cf82f46
ssabunayeem <ssabunayeem@gmail.com>
2025-12-20 04:12:15 +0600

    ipdate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -15,33 +15,43 @@ const Home = () => {
   return (
     <div>
 
-      <Banner></Banner>
-
-      <div className='flex flex-col justify-center items-center max-w-[1480px] mx-auto  md:px-8 lg:px-12 md:py-8 lg:py-12'>
-        <h1 className='text-5xl font-bold'>Trending Apps</h1>
-        <p className='text-gray-500 my-5'>Explore All Trending Apps on the Market developed by us</p>
-      </div>
-
-
-      <div className='max-w-[1480px] mx-auto'>
+      <div className=''>
         {loading ? (
           <SkeletonLoader />
         ) : (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-            {featuredProducts.map(product => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+          <div>
+            <Banner></Banner>
+
+            <div className='flex flex-col justify-center items-center max-w-[1480px] mx-auto  md:px-8 lg:px-12 md:py-8 lg:py-12'>
+              <h1 className='text-5xl font-bold'>Trending Apps</h1>
+              <p className='text-gray-500 my-5'>Explore All Trending Apps on the Market developed by us</p>
+            </div>
+
+
+            <div className='max-w-[1480px] mx-auto'>
+              {loading ? (
+                <SkeletonLoader />
+              ) : (
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                  {featuredProducts.map(product => (
+                    <ProductCard key={product.id} product={product} />
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <div className='max-w-[1480px] mx-auto text-center my-20'>
+              <Link className='btn btn-outline bg-linear-65 from-violet-700 to-purple-500 text-white px-10' to='/products'>
+                Show All
+              </Link>
+            </div>
           </div>
         )}
-
-
       </div>
 
-      <div className='max-w-[1480px] mx-auto text-center my-20'>
-        <Link className='btn btn-outline bg-linear-65 from-violet-700 to-purple-500 text-white px-10' to='/products'>
-          Show All
-        </Link>
-      </div>
+
+
+
 
     </div>
   )
